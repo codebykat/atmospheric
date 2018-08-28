16f0e820bac0ab18f8e1f71d2b125c066fdc08fb
Kat Hagan <kat@codebykat.com>
2018-08-28 14:51:29 -0700

    subdirectories

--- generate.py
+++ generate.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import glob
 import jinja2
-from os import path
+import os
 from PIL import Image
 from tqdm import tqdm # fancy progress bar 😍
 
@@ -15,21 +15,41 @@ exportFolder = 'cloud/';
 
 thumbnail_sizes = [ (100, 100), (250, 250), (500, 500) ]
 
+def get_image_folder( uid, relative=True ):
+	if relative:
+		return exportFolder + uid + '/'
+	return exportRoot + exportFolder + uid + '/'
+
+def get_image_filename( uid, size='full' ):
+	return uid + '-' + size + '.jpg'
+
+def get_image_path( uid, size='full', relative=True ):
+	return get_image_folder( uid, relative ) + get_image_filename( uid, size )
+
 clouds = []
 print( "Processing new clouds..." )
 for file in tqdm( glob.glob( importFolder + '*.jpg' ) ):
-	size = 50, 50
+	uid = os.path.splitext( os.path.basename ( file ) )[0]
+
+	image_folder = get_image_folder( uid, False )
+	if not os.path.exists( image_folder ):
+		os.makedirs( image_folder )
+
 	img = Image.open( file )
+
+	# copy full-sized image into export folder
+	img.save( get_image_path( uid, 'full', False ), "JPEG" )
+
+	# generate and save thumbnail
+	size = 50, 50
 	img.thumbnail( size )
 
-	filename = path.basename( file )
-	file_url = exportFolder + filename
+	img.save( get_image_path( uid, 'thumb', False ), "JPEG" )
 
-	thumbnail_path = exportRoot + file_url + '.thumbnail'
-	img.save( thumbnail_path, "JPEG" )
-
-	thumbnail_url = file_url + '.thumbnail'
-	clouds.append( { 'url': file_url, 'thumbnail': thumbnail_url } )
+	clouds.append( {
+		'url': get_image_path( uid ),
+		'thumbnail': get_image_path( uid, 'thumb' )
+	} )
 
 # clouds = [ {
 # 	'url': file,
@@ -39,6 +59,7 @@ for file in tqdm( glob.glob( importFolder + '*.jpg' ) ):
 # output folder structure = by date?
 
 # write a top-level page with an index
+print ( "Writing index page..." )
 template = templateEnv.get_template( 'index.html.j2' )
 
 with open( exportRoot + '/index.html', 'w' ) as index_html:
